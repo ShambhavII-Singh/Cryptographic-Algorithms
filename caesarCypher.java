238c53c8ba8c759044bdc03a7eb1b8f37bba4089
ShambhavII-Singh <98341003+ShambhavII-Singh@users.noreply.github.com>
2024-01-25 14:04:16 +0530

    Create caesarCypher.java

---
+++ caesarCypher.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.text.CharacterIterator; 
+import java.text.StringCharacterIterator; 
+
+public class Main {
+
+    public static String caesarCypherEncryption(String message, int key) {
+
+        int len = message.length();
+        char[] answer = message.toCharArray();
+        int pointer = 0;
+        CharacterIterator itr = new StringCharacterIterator(message); 
+
+        while (itr.current() != CharacterIterator.DONE) {
+            if (itr.current()==' ') {
+                answer[pointer] = ' ';
+            }
+            else {
+                int isolated = (int) itr.current() + key;
+                if (isolated<=90) {
+                    answer[pointer] = (char) isolated;
+                }
+                else {
+                    isolated = (isolated % 91) + 65;
+                    answer[pointer] = (char) isolated;
+                }
+            }
+            pointer++;
+            itr.next();
+        } 
+
+        System.out.print("ENCRYPTED MESSAGE IS : ");
+        System.out.println(answer);
+
+        String encrypted = new String(answer);
+        return encrypted;
+
+    }
+  
+}
